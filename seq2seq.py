02da0496d17f7b5fb8f97977ea6947bb1220cd2e
sunshinFan <chni@eng.ucsd.edu>
2019-03-17 18:12:58 -0700

    Add files via upload

--- seq2seq.py
+++ seq2seq.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from Encoder import Encoder
-from AttnDecoderRNN import AttnDecoderRNN
 from DecoderRNN import DecoderRNN
 
 
@@ -9,12 +8,10 @@ class seq2seq(nn.Module):
     def __init__(self, word_size_encoder, emb_dim, hidden_size, word_size_decoder):
         super(seq2seq, self).__init__()
         self.encoder = Encoder(word_size_encoder, emb_dim, hidden_size)
-        # self.decoder = AttnDecoderRNN(hidden_size, word_size_decoder)
         self.decoder = DecoderRNN(hidden_size, word_size_decoder)
 
     # input_encoder : vector of index
     def forward(self, input_encoder, input_decoder):
-        output, hidden = self.encoder(input_encoder)
-        #print(hidden)
-        output = self.decoder(input_decoder, hidden)
+        output, hidden, hidden_list = self.encoder(input_encoder)
+        output = self.decoder(input_decoder, hidden, hidden_list)
         return output
