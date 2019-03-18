f5eb6c272c94170f43c157e2923bb6c2a5857a5a
sunshinFan <chni@eng.ucsd.edu>
2019-03-17 18:10:29 -0700

    Add files via upload
    with hidden_state list

--- Encoder.py
+++ Encoder.py
@@ -15,9 +15,11 @@ class Encoder(nn.Module):
     # emb_dim : the dimension to represent one vocabulary
     def __init__(self, word_size, emb_dim, hidden_size, device=None):
         super(Encoder, self).__init__()
-        self.embedding = nn.Embedding(word_size, emb_dim)
-        self.lstm = nn.LSTM(input_size=word_size* emb_dim, hidden_size=hidden_size)
+        self.embeddings = nn.Embedding(word_size, emb_dim)
+        self.lstm = nn.LSTM(input_size=word_size * emb_dim, hidden_size=hidden_size)
         self.hidden = self._init_hidden(hidden_size, device)
+        self.hidden_list = []
+        self.hidden_list.append(self.hidden)
 
     # input is an index vector
     def forward(self, input):
@@ -26,14 +28,13 @@ class Encoder(nn.Module):
 
         # output.size = number of features
         output, self.hidden = self.lstm(output, self.hidden)
+        self.hidden_list.append(self.hidden)
 
         # hidden state will be used to compute attention
-        return output, self.hidden
+        return output, self.hidden, self.hidden_list
 
     def _init_hidden(self, hidden_size, device):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        return [torch.zeros([1, 1, hidden_size]).to(device),
-                torch.zeros([1, 1, hidden_size]).to(device)]
+        return torch.zeros(1, 1, hidden_size).to(device)
 
 
 
