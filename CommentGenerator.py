6be48429f4645e4e9e91a5371edc4cca7257323d
Weifeng Hu <weifenghu@weifengs-mbp.home>
2019-03-20 18:48:47 -0700

    show attention

--- CommentGenerator.py
+++ CommentGenerator.py
@@ -3,6 +3,9 @@ import pickle
 import torch
 import numpy as np
 # import Encoder
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
 class Generator:
     def __init__(self, encoder, decoder, comment_dict, SOS_token, EOS_token, device, max_length = 20):
         self.dict = {}
@@ -60,12 +63,29 @@ class Generator:
         return res
 
 def load_model(type='attn'):
-    with open(type+'_encoder.ckpt', 'rb') as pfile:
+    with open(type+'_encoder1.ckpt', 'rb') as pfile:
         encoder = pickle.load(pfile)
-    with open(type + '_decoder.ckpt', 'rb') as pfile:
+    with open(type + '_decoder1.ckpt', 'rb') as pfile:
         decoder = pickle.load(pfile)
     return encoder, decoder
 
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.savefig('attention_map.png')
 
 
 def main():
@@ -94,6 +114,7 @@ def main():
     _output = generator(_input,_target)
     print(generator.generate(_target))
     print(_output)
+    showAttention(_input, _output, decoder.attn.weights)
 
 
 if __name__ == '__main__':
