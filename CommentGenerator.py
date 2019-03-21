5dd6c37bfa6068b9e15ce8c84d5eb3c5badf76c3
Weifeng Hu <weifenghu@weifengs-mbp.home>
2019-03-20 18:57:07 -0700

    weight

--- CommentGenerator.py
+++ CommentGenerator.py
@@ -77,9 +77,9 @@ def showAttention(input_sentence, output_words, attentions):
     fig.colorbar(cax)
 
     # Set up axes
-    ax.set_xticklabels([''] + input_sentence.split(' ') +
-                       ['<EOS>'], rotation=90)
-    ax.set_yticklabels([''] + output_words)
+    # ax.set_xticklabels([''] + input_sentence.split(' ') +
+    #                    ['<EOS>'], rotation=90)
+    # ax.set_yticklabels([''] + output_words)
 
     # # Show label at every tick
     # ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
